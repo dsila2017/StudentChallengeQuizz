df014acf93d7c129d7cc58f261bad8f938ac7d38
dsila2017 <you@example.com>
2024-02-25 23:36:21 +0400

    Initial Commit

---
+++ StudentChallengeQuizz/StudentChallengeQuizzApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudentChallengeQuizzApp.swift
+//  StudentChallengeQuizz
+//
+//  Created by David on 2/25/24.
+//
+
+import SwiftUI
+
+@main
+struct StudentChallengeQuizzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
